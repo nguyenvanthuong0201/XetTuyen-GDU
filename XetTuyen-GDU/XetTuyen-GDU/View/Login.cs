31d91a525eaaeee15d1d076d86470a11d4fadd21
nguyenvanthuong0201 <55219637+nguyenvanthuong0201@users.noreply.github.com>
2020-04-25 16:37:12 +0700

    Commit

---
+++ XetTuyen-GDU/XetTuyen-GDU/View/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace XetTuyen_GDU.View
+{
+    public partial class Login : DevExpress.XtraEditors.XtraForm
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
